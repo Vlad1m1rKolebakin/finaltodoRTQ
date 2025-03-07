4aed7256fe82f8e861ddff3b3aca485ac8841f70
Vlad1m1rKolebakin <kolebakin.v@yandex.ru>
2025-03-07 13:04:57 +0300

    errorcomitHandle

--- src/common/utils/handleError.ts
+++ src/common/utils/handleError.ts
@@ -1,40 +1,81 @@
+// import { BaseQueryApi, FetchBaseQueryError, FetchBaseQueryMeta, QueryReturnValue } from "@reduxjs/toolkit/query"
+// import { setAppError } from "../../app/appSlice"
+// import { ResultCode } from "../enums/enums"
+
 import { BaseQueryApi, FetchBaseQueryError, FetchBaseQueryMeta, QueryReturnValue } from "@reduxjs/toolkit/query"
 import { setAppError } from "../../app/appSlice"
 import { ResultCode } from "../enums/enums"
 
-  export const handleError = (
-    api: BaseQueryApi,
-    result: QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>
-  ) => {
-    let error = 'Some error occurred'
+//   export const handleError = (
+//     api: BaseQueryApi,
+//     result: QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>
+//   ) => {
+//     let error = 'Some error occurred'
    
-    if (result.error) {
-      switch (result.error.status) {
-        case 'FETCH_ERROR':
-        case 'PARSING_ERROR':
-        case 'CUSTOM_ERROR':
-          error = result.error.error
-          break
+//     if (result.error) {
+//       switch (result.error.status) {
+//         case 'FETCH_ERROR':
+//         case 'PARSING_ERROR':
+//         case 'CUSTOM_ERROR':
+//           error = result.error.error
+//           break
    
-        case 403:
-          error = '403 Forbidden Error. Check API-KEY'
-          break
+//         case 403:
+//           error = '403 Forbidden Error. Check API-KEY'
+//           break
    
-        case 400:
-        case 500:
-          error = (result.error.data as { message: string }).message
-          break
+//         case 400:
+//         case 500:
+//           error = (result.error.data as { message: string }).message
+//           break
    
-        default:
-          error = JSON.stringify(result.error)
-          break
-      }
-      api.dispatch(setAppError({ error }))
+//         default:
+//           error = JSON.stringify(result.error)
+//           break
+//       }
+//       api.dispatch(setAppError({ error }))
+//     }
+   
+//     if ((result.data as { resultCode: ResultCode }).resultCode === ResultCode.Error) {
+//       const messages = (result.data as { messages: string[] }).messages
+//       error = messages.length ? messages[0] : error
+//       api.dispatch(setAppError({ error }))
+//     }
+//   }
+
+export const handleError = (
+  api: BaseQueryApi,
+  result: QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>,
+) => {
+  let error = "Some error occurred"
+
+  if (result.error) {
+    switch (result.error.status) {
+      case "FETCH_ERROR":
+      case "PARSING_ERROR":
+      case "CUSTOM_ERROR":
+        error = '401'
+        break
+
+      case 403:
+        error = "403 Forbidden Error. Check API-KEY"
+        break
+
+      case 400:
+      case 500:
+        error = (result.error.data as { message: string }).message
+        break
+
+      default:
+        error = JSON.stringify(result.error)
+        break
     }
-   
-    if ((result.data as { resultCode: ResultCode }).resultCode === ResultCode.Error) {
-      const messages = (result.data as { messages: string[] }).messages
-      error = messages.length ? messages[0] : error
-      api.dispatch(setAppError({ error }))
-    }
+    api.dispatch(setAppError({ error }))
   }
+
+  if ((result.data as { resultCode: ResultCode }).resultCode === ResultCode.Error) {
+    const messages = (result.data as { messages: string[] }).messages
+    error = messages.length ? messages[0] : error
+    api.dispatch(setAppError({ error }))
+  }
+}
